0cc50102e4b049372af54874f7fc618689be1521
Mayank Pradhan <65954185+myankprad@users.noreply.github.com>
2023-04-12 07:11:58 +0000

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./src/app.js"
+
+( async ()=> {
+  try{
+     mongoose.connect(config.MONGODB_URL)
+     console.log("DB CONNECTED");
+     app.on("error" ,(err)=>{
+        console.error("error", err);
+        throw err 
+     } {
+
+     })
+  }
+}
+)
