646331b34a9e31ed36357ddecd19daf0e5378dfb
Mayank Pradhan <65954185+myankprad@users.noreply.github.com>
2023-04-18 07:43:29 +0000

    commit 2

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles";
 import bcrypt from "bcrypt.js"
+import config from "../config";
+import crypto from "crypto"
+import { Jwt } from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -37,8 +40,42 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function(next){
     if (this.isModified("password")) return next()
-    bcrypt.hash(this.password, 10)
-}  )
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods = {
+    //  compare the password
+
+    comparePassword: async function(enteredPassword){
+       return await bcrypt.compare(enteredPassword, this.password)
+    },
+    //  generate jwt token
+    getJWTtoken : function(){
+        JWT.sign({_id: this._id, role: this.role}, config.JWT_SECRET,
+         {
+            expiresIn : config.JWT_EXPIRY
+        })
+    },
+
+    // generate forgot password token 
+    generateForgotPasswordToken : function(){
+       const forgotToken =   crypto.randomBytes(20).toString("hex")
+
+    // encrypt the token generate by crypto     
+
+       this.forgotPasswordToken = crypto
+       .createHash("sha256")
+       .update(forgotToken)
+       .digest("hex")
+
+    //    time to token to expire
+     this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000
+     return forgotToken
+    }
+}
+
+
 
 
 
