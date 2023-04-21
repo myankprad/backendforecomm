2e325eb960fb84e9b9e23fa13465e13e770263b9
Mayank Pradhan <65954185+myankprad@users.noreply.github.com>
2023-04-21 05:13:52 +0000

    routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
-import { express } from "express";
+ 
+
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import routes from "./routes/index.js"
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use
+app.use(cors())
+app.use(cookieParser())
+
+app.use("/api/v1/", routes)
+
+app.get("/", (_req, res) => {
+    res.send("Hello there hitesh - API")
+})
+
+
+app.all("*", (_req, res) => {
+    return res.status(404).json({
+        success: false,
+        message: "Route not found"
+    })
+})
 
 export default app;
+//  _req here " _ " means we are not using it
+// http://localhost:4000/api/v1/auth/login
+// http://localhost:4000/api/v1/auth/logout
