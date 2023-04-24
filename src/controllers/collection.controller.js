36f82d0273070818766e4fac1398496163ec0131
Mayank Pradhan <65954185+myankprad@users.noreply.github.com>
2023-04-24 10:40:06 +0000

    delete and update coupon

--- src/controllers/collection.controller.js
+++ src/controllers/collection.controller.js
@@ -76,3 +76,47 @@ export const getAllCollection = asyncHandler(async(req, res)=>{
         collections
     })
 })
+
+export const updateCoupon = asyncHandler(async (req, res) => {
+    const {id: couponId} = req.params
+    const {action} = req.body
+
+    // action is boolean or not
+
+    const coupon = await Coupon.findByIdAndUpdate(
+        couponId,
+        {
+            active: action
+        }, 
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+    if (!coupon) {
+        throw new CustomError("Coupon not found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Coupon updated",
+        coupon
+    })
+})
+
+export const deleteCoupon = asyncHandler(async (req, res)=>{
+    const {id: couponId} = req.params
+    
+   const coupon = await Coupon.findByIdAndDelete(couponId)
+
+   if (!coupon) {
+    throw new CustomError("Coupon not found", 404)
+}
+
+res.status(200).json({
+    success: true,
+    message: "Coupon deleted",
+    coupon
+})
+
+})
